62c677e6535e9a5fdb6f5a7ee384b603eee5c720
f b <jehronbuarao1121@gmail.com>
2026-05-02 18:46:12 +0800

    Done on ADDING SECTION, Database not yet

--- lib/screens/adding/adding_section.dart
+++ lib/screens/adding/adding_section.dart
@@ -4,6 +4,9 @@ import '../../designs/drawer.dart';
 import '../../designs/themes.dart';
 import '../../designs/appbar.dart';
 import 'package:image_picker/image_picker.dart'; // Add new logic function
+import 'dart:io';
+import 'barcode_scanner_page.dart';
+
 
 class AddingSectionPage extends ConsumerStatefulWidget {
   const AddingSectionPage({super.key});
@@ -13,7 +16,96 @@ class AddingSectionPage extends ConsumerStatefulWidget {
 }
 
 class _AddingSectionPageState extends ConsumerState<AddingSectionPage> {
-  @override
+
+final ImagePicker _picker = ImagePicker();
+File? _selectedImage;
+
+final TextEditingController barcodeController = TextEditingController();
+
+final TextEditingController _dateContoller = TextEditingController();
+
+Future<void> _pickImage(ImageSource source) async {
+  // Use ImageSource.gallery or ImageSource.camera
+  final XFile? pickedFile = await _picker.pickImage(
+    source: source,
+    maxWidth: 1800, // Optional: limit size
+    imageQuality: 80, // Optional: compress image
+  );
+
+  if (pickedFile != null) {
+    setState(() {
+      _selectedImage = File(pickedFile.path);
+    });
+  }
+}  
+
+void _removePhoto() {
+  setState(() {
+    _selectedImage = null;
+  });
+}
+
+
+  void _showActionDialog() {
+  showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        title: Text('Upload Photo'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              leading: Icon(Icons.camera_alt),
+              contentPadding: EdgeInsets.only(left: -8),
+              title: Text('Camera'),
+              onTap: () {
+                Navigator.pop(context); _pickImage(ImageSource.camera);
+                // TODO: camera action
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.photo),
+                 contentPadding: EdgeInsets.only(left: -8),
+              title: Text('Gallery'),
+              onTap: () {
+                Navigator.pop(context); _pickImage(ImageSource.gallery);
+                // TODO: gallery action
+              },
+            ),
+             ListTile(
+              leading: Icon(Icons.delete),
+                 contentPadding: EdgeInsets.only(left: -8),
+              title: Text('Remove Photo'),
+              onTap: () {
+                Navigator.pop(context); _removePhoto();
+                // TODO: gallery action
+              },
+            ),
+          ],
+        ),
+      );
+    },
+  );
+}
+
+Future<void> _selectDate() async{
+  DateTime? _picked = await showDatePicker(
+    context: context,
+    initialDate: DateTime.now(),
+    firstDate: DateTime(2000), 
+    lastDate: DateTime(2100)
+    );
+    if (_picked != null){
+      setState(() {
+      _dateContoller.text = _picked.toString().split(" ")[0];
+      });
+    }
+}
+
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
@@ -29,82 +121,80 @@ class _AddingSectionPageState extends ConsumerState<AddingSectionPage> {
             Row(
   crossAxisAlignment: CrossAxisAlignment.start,
   children: [
-    // LEFT SIDE (Upload UI)
+    // LEFT SIDE
     Expanded(
       flex: 1,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          SizedBox(height: 25),
-         SizedBox(
-  height: 100,
-  width: double.infinity,
-  child: ElevatedButton(
-    style: ElevatedButton.styleFrom(
-      elevation: 0, // remove default shadow if you want flat look
-      backgroundColor: const Color.from(alpha: 0, red: 0, green: 0, blue: 0),
-      foregroundColor: const Color.fromARGB(255, 38, 15, 144),
-      side: BorderSide(color:  Color.fromARGB(255, 38, 15, 144)), // border
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8),
-      ),
-    ),
-    onPressed: () {
-      // TODO: add upload action
-    },
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(
-          Icons.add_a_photo,
-          size: 28.0,
-          color: const Color.fromARGB(255, 38, 15, 144),
-        ),
-        SizedBox(height: 3),
-        Text(
-          'Photo',
-          style: TextStyle(
-            color: const Color.fromARGB(255, 38, 15, 137),
+          const SizedBox(height: 25),
+
+          GestureDetector(
+            onTap: _showActionDialog,
+            child: Container(
+              height: 100,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: const Color.fromARGB(255, 38, 15, 144),
+                ),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              clipBehavior: Clip.hardEdge,
+
+              child: _selectedImage != null
+                  ? Image.file(
+                      _selectedImage!,
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                      height: double.infinity,
+                    )
+                  : const Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.add_a_photo,
+                            size: 28,
+                            color: Color.fromARGB(255, 38, 15, 144),
+                          ),
+                          SizedBox(height: 5),
+                          Text("Photo"),
+                        ],
+                      ),
+                    ),
+            ),
           ),
-        ),
-      ],
-    ),
-  ),
-),
         ],
       ),
     ),
 
-    SizedBox(width: 15),
+    const SizedBox(width: 15),
 
-    // RIGHT SIDE (Name + Description)
+    // RIGHT SIDE
     Expanded(
       flex: 2,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('PRODUCT NAME'),
-          SizedBox(height: 5),
+          const Text('PRODUCT NAME'),
+          const SizedBox(height: 5),
           TextField(
             decoration: InputDecoration(
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.grey),
-              ),
+              border: OutlineInputBorder(),
               labelText: 'Enter product name',
             ),
           ),
 
-          SizedBox(height: 15),
+          const SizedBox(height: 15),
 
-          Text('DESCRIPTION'),
-          SizedBox(height: 5),
+          const Text('DESCRIPTION'),
+          const SizedBox(height: 5),
           TextField(
             minLines: 1,
-            maxLines: null, // makes description taller
+            maxLines: null,
             decoration: InputDecoration(
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.grey),
-              ),
+              border: OutlineInputBorder(),
               labelText: 'Tap to add description',
             ),
           ),
@@ -113,17 +203,64 @@ class _AddingSectionPageState extends ConsumerState<AddingSectionPage> {
     ),
   ],
 ),
-                SizedBox(height: 20),
-                Text('BARCODE NUMBER'),
-                SizedBox(height: 5.0),
-                TextField(
-                  decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey),
-                    ),
-                    labelText: 'Barcode number...',
-                  ),
-                ),
+
+  SizedBox(height: 20),
+
+Text('BARCODE NUMBER'),
+
+SizedBox(height: 10),
+
+Stack(
+  clipBehavior: Clip.none,
+  children: [
+    TextField(
+      controller: barcodeController,
+      decoration: InputDecoration(
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        labelText: 'Barcode number...',
+      ),
+    ),
+
+    Positioned(
+      right: 12,
+      top: 8,
+      child: Material(
+        elevation: 1,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12),),
+        color: Colors.white,
+        child: InkWell(
+          onTap: () async {
+  final result = await Navigator.push(
+    context,
+    MaterialPageRoute(
+      builder: (context) => const BarcodeScannerPage(),
+    ),
+  );
+
+  if (result != null) {
+    setState(() {
+       barcodeController.text = result;// assign to your text field controller or variable
+      print("Scanned: $result");
+    });
+  }
+},
+          customBorder: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12),),
+          child: Padding(
+            padding: EdgeInsets.all(6),
+            child: Icon(
+              Icons.qr_code_scanner,
+              color: Colors.black,
+            ),
+          ),
+        ),
+      ),
+    ),
+  ],
+),
                 SizedBox(height: 20.0),
                 Row(
                   children: [
@@ -156,6 +293,7 @@ class _AddingSectionPageState extends ConsumerState<AddingSectionPage> {
                     SizedBox(width: 10.0), // Second TextField
                     Flexible(
                       child: TextField(
+                        controller: _dateContoller,
                         decoration: InputDecoration(
                           labelText: 'Select Date',
                           filled: true,
@@ -166,6 +304,9 @@ class _AddingSectionPageState extends ConsumerState<AddingSectionPage> {
                           ),
                         ),
                         readOnly: true,
+                        onTap: () {
+                          _selectDate();
+                        },
                       ),
                     ),
                   ],
@@ -204,13 +345,11 @@ class _AddingSectionPageState extends ConsumerState<AddingSectionPage> {
                       child: TextField(
                         decoration: InputDecoration(
                           labelText: 'P 00.0',
-                          filled: true,
                           fillColor: Colors.transparent,
                           enabledBorder: OutlineInputBorder(
                             borderSide: BorderSide(color: Colors.grey),
                           ),
                         ),
-                        readOnly: true,
                       ),
                     ),
                   ]
@@ -257,5 +396,10 @@ Row(
         ),
       ), // left, top, right, bottom,),
     );
+  }
+  @override
+void dispose() {
+  barcodeController.dispose();
+  super.dispose();
   }
 }
