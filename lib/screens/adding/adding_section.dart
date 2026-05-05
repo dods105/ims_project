0ed40b6f3e8bf594012f6199c3152204439263c6
Stacy Jordan <laranyxer@gmail.com>
2026-05-05 20:07:43 +0800

    adding section db added

--- lib/screens/adding/adding_section.dart
+++ lib/screens/adding/adding_section.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: unused_field
-
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/models/products/products.dart';
 import 'package:flutter_application_1/providers/auth_provider.dart';
